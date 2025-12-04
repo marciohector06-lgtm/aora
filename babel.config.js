54d8acc9e7ad8ba10089ce3d4bfdfd62ac546d0b
Márcio Sodré <marciohector.06@gmail.com>
2025-12-04 14:59:58 -0300

    new pacto

--- babel.config.js
+++ babel.config.js
@@ -5,3 +5,20 @@ module.exports = function (api) {
     plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
   };
 };
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+    ]
+  };
+};
